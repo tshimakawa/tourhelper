55ca1806a5b54a188782f9b8ed4008380c764c69
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-09 17:20:23 +0900

    app.jsを改良しました

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +27,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+//HTTPS通信で使用するためのSSLキーを設定
+const ssloptions = {
+        key: fs.readFileSync ('./sslKey/privkey5.pem'),
+        cert: fs.readFileSync('./sslKey/cert5.pem'),
+        ca: [fs.readFileSync('./sslKey/chain5.pem'), fs.readFileSync('./sslKey/fullchain5.pem','utf-8')],
+requestCert: true,
+rejectUnauthorized: false
+};
+
+mongoose.Promise = global.Promise;
+const mongodbUri = 'mongodb://localhost/household_account';
+const mongOptions = {
+    useMongoClient: true,
+    socketTimeoutMS: 0,
+    keepAlive: true,
+    reconnectTries: 30
+};
+
+//ルーティングの設定 各エンドポイントに対応したルーティング先に誘導
+app.use('/', index);
+app.use('/users', users);
+//app.login('/login',login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,6 +67,15 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// ポート設定
+app.set('httpsport', process.env.PORT || 5000);
+
+// サーバ立ち上げ
+var server = https.createServer(ssloptions,app).listen(app.get('httpsport'), function(){
+    console.log('Express HTTPS server listening on port ' + app.get('httpsport'));
+    mongoose.connect(mongodbUri, mongOptions);
 });
 
 module.exports = app;

70687fe0200ca47e649a77d661136e919273a67e
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-16 00:45:47 +0900

    places API　テスト

--- routes/index.js
+++ routes/index.js
@@ -26,10 +26,6 @@ module.exports = router;
 
 async function test(){
   console.log("accessed test");
-  // const spot_list = await allmodule.google_api.places_api();
-  setTimeout(() => {
-    console.log("api終了");
-    console.log(await allmodule.google_api.places_api());
-    console.log("test終了");
-}, 4000);
+  const spot_list = await allmodule.google_api.places_api();
+  console.log(spot_list);
 }

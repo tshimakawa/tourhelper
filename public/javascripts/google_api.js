285ea362d145292ac24a5ae4f0c3fd8fcc630a06
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-16 03:49:26 +0900

    places API　テスト

--- public/javascripts/google_api.js
+++ public/javascripts/google_api.js
@@ -4,9 +4,44 @@ var aaaaaa;
 
 exports.places_api = async function(){
   console.log("accessed google_api.js");
-  const result = await asyncFunction();
+  async.waterfall([
+    function(callback) {
+      console.log("$$$$$$$$");
+  // 同期通信でGETリクエスト
+  request.get({
+    url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
+    headers:{
+      'Content-Type':'text/plain;charset=utf-8'
+    },
+    qs:{
+      key:'AIzaSyDnjy1JCD2XQNej0kWaGnXN_VNrhxvmogs',
+      location:'35,135',
+      radius:'50000',
+      keyword:'水族館',
+    },
+    json:true
+  }, function (error, response, body) {
+    console.log('##########################');
+    const result = body.results;
+    const spot_list = [];
+    for (var i=0;i<result.length;i++){
+      const spot = {};
+      spot.latitude = result[i].geometry.location.lat;
+      spot.longitude = result[i].geometry.location.lng;
+      spot.name = result[i].name;
+      spot_list[i] = spot;
+    }
+    callback(null,spot_list);
+  });
+}],function(err, result) {
+  if (err) {
+    throw err;
+  }
+  console.log('----------------------------------------------------');
+  console.log(result);
+  return result;
+});
   console.log('!!!!!!!!!!!!!!');
-  return 1000;
   //探索範囲をドライブ時間から計算
   // const starttime = parameters.starttime;
   // const endtime = parameters.endtime;
@@ -50,7 +85,6 @@ async function asyncFunction() {
         throw err;
       }
       console.log('----------------------------------------------------');
-      aaaaaa = result;
       return result;
     });
 }

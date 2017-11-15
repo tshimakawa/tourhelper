01fbee2ec78b6d2831cdb5599f04f7aedc6679c7
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-15 22:52:55 +0900

    places API　テスト

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const allmodule = require('../public/javascripts');
 router.get('/', function(req, res, next) {
   async.waterfall([
   function(callback) {
-    const spot_list = allmodule.google_api.places_api();
-    callback(null,spot_list);
+    const spot_list = async () => allmodule.google_api.places_api();
+    await callback(null,spot_list);
   }],function(err, result) {
     if (err) {
       throw err;

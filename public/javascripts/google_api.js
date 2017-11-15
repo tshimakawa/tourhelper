ea2e62c843f90cf77de1161678d61c7dcdc01235
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-16 00:38:08 +0900

    places API　テスト

--- public/javascripts/google_api.js
+++ public/javascripts/google_api.js
@@ -22,43 +22,6 @@ exports.places_api = async function(){
   });
 }
 
-function test(){
-  async.waterfall([
-    function(callback) {
-  // 同期通信でGETリクエスト
-  request.get({
-    url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
-    headers:{
-      'Content-Type':'text/plain;charset=utf-8'
-    },
-    qs:{
-      key:'AIzaSyDnjy1JCD2XQNej0kWaGnXN_VNrhxvmogs',
-      location:'35,135',
-      radius:'50000',
-      keyword:'水族館',
-    },
-    json:true
-  }, function (error, response, body) {
-    const result = body.results;
-    const spot_list = [];
-    for (var i=0;i<result.length;i++){
-      const spot = {};
-      spot.latitude = result[i].geometry.location.lat;
-      spot.longitude = result[i].geometry.location.lng;
-      spot.name = result[i].name;
-      spot_list[i] = spot;
-    }
-    callback(null,spot_list);
-  });
-}],function(err, result) {
-  if (err) {
-    throw err;
-  }
-  console.log('----------------------------------------------------');
-  return result;
-});
-}
-
 function asyncFunction() {
     // Promiseオブジェクトを返却する.処理成功時にはresolveが呼ばれる
     return new Promise(function (resolve, reject) {
@@ -96,7 +59,7 @@ function asyncFunction() {
       }
       console.log('----------------------------------------------------');
       aaaaaa = result;
-    });
       resolve('Async Hello world');
+    });
     });
 }

ad2915f676514b72186a25b03467d1e46f55f0ec
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-15 19:51:42 +0900

    places API　テスト

--- public/javascripts/google_api.js
+++ public/javascripts/google_api.js
@@ -17,6 +17,9 @@ exports.places_api=function(){
     // 同期通信でGETリクエスト
     request.get({
       url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
+      headers:{
+        'Content-Type', 'text/plain;charset=utf-8'
+      }
       qs:{
         location:'135,35',
         radius:'50000',

eab75cee072e647e03412857fad9ba9296476e3e
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-15 23:06:56 +0900

    places API　テスト

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,21 @@ const allmodule = require('../public/javascripts');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  async.waterfall([
-  async function(callback) {
-      const spot_list = await allmodule.google_api.places_api();
-      callback(null,spot_list);
-  }],function(err, result) {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-    res.render('index', { title: 'Express' });
-  });
+  // async.waterfall([
+  // async function(callback) {
+  //     const spot_list = await allmodule.google_api.places_api();
+  //     callback(null,spot_list);
+  // }],function(err, result) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   console.log(result);
+  //   res.render('index', { title: 'Express' });
+  // });
+  async function test(){
+    const spot_list = await allmodule.google_api.places_api();
+    console.log(spot_list);
+  }
 });
 
 module.exports = router;

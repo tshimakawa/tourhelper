d63ddde663b82d2bea2ab67e4e5e5b324b7f907c
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-15 21:13:42 +0900

    places API　テスト

--- public/javascripts/google_api.js
+++ public/javascripts/google_api.js
@@ -37,6 +37,7 @@ exports.places_api=function(){
         spot.longitude = result[i].geometry.location.lng;
         spot.name = result[i].name;
         spot.icon = result[i].icon;
+        console.log(spot);
         spot_list.push(spot);
       }
       callback(null,spot_list);

fd4fa3fd85e43d63d49561905daf24af19819884
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-16 03:41:17 +0900

    places API　テスト

--- public/javascripts/google_api.js
+++ public/javascripts/google_api.js
@@ -20,7 +20,7 @@ async function asyncFunction() {
         function(callback) {
           console.log("$$$$$$$$");
       // 同期通信でGETリクエスト
-      request.get({
+      await request.get({
         url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
         headers:{
           'Content-Type':'text/plain;charset=utf-8'

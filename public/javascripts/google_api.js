bc4c5a5792d66a9aa431ede828d86955a86cdeb2
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-16 00:14:10 +0900

    places API　テスト

--- public/javascripts/google_api.js
+++ public/javascripts/google_api.js
@@ -1,7 +1,7 @@
 const async = require('async');
 const request = require("request");
 
-exports.places_api = function(){
+exports.places_api = async function(){
 
   console.log("accessed google_api.js");
 
@@ -11,43 +11,45 @@ exports.places_api = function(){
   // const latitude = parameters.latitude;
   // const longitude = parameters.longitude;
 
-  const result = async function(){
-  async.waterfall([
-  function(callback) {
-    // 同期通信でGETリクエスト
-    request.get({
-      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
-      headers:{
-        'Content-Type':'text/plain;charset=utf-8'
-      },
-      qs:{
-        key:'AIzaSyDnjy1JCD2XQNej0kWaGnXN_VNrhxvmogs',
-        location:'35,135',
-        radius:'50000',
-        keyword:'水族館',
-      },
-      json:true
-    }, function (error, response, body) {
-      const result = body.results;
-      const spot_list = [];
-      for (var i=0;i<result.length;i++){
-        const spot = {};
-        spot.latitude = result[i].geometry.location.lat;
-        spot.longitude = result[i].geometry.location.lng;
-        spot.name = result[i].name;
-        spot_list[i] = spot;
-      }
-      callback(null,spot_list);
-    });
-  }],function(err, result) {
-    if (err) {
-      throw err;
-    }
-    console.log('----------------------------------------------------');
-    return result;
-  });
-}
+  const result = await test();
   console.log('all done.');
   console.log(result);
   return result;
+}
+
+function test(){
+  async.waterfall([
+    function(callback) {
+  // 同期通信でGETリクエスト
+  request.get({
+    url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
+    headers:{
+      'Content-Type':'text/plain;charset=utf-8'
+    },
+    qs:{
+      key:'AIzaSyDnjy1JCD2XQNej0kWaGnXN_VNrhxvmogs',
+      location:'35,135',
+      radius:'50000',
+      keyword:'水族館',
+    },
+    json:true
+  }, function (error, response, body) {
+    const result = body.results;
+    const spot_list = [];
+    for (var i=0;i<result.length;i++){
+      const spot = {};
+      spot.latitude = result[i].geometry.location.lat;
+      spot.longitude = result[i].geometry.location.lng;
+      spot.name = result[i].name;
+      spot_list[i] = spot;
+    }
+    callback(null,spot_list);
+  });
+}],function(err, result) {
+  if (err) {
+    throw err;
+  }
+  console.log('----------------------------------------------------');
+  return result;
+});
 }

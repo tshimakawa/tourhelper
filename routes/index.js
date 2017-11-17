6deea612be75190d82ba71ce1caad3bd7baf7893
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-11-17 12:56:26 +0900

    TwitterAPIのテスト

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
       allmodule.twitter_api.twitter_api(places_api_result).then(
         function(twitter_api_result){
           console.log("twitter_apiの結果です");
-          // console.log(twitter_api_result);
+          console.log(twitter_api_result);
           res.render('index', { title: 'Express' });
         },function(error){
           console.log(error);

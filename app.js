096560066a71fc95390bfff6d87910b3840a7f63
tshimakawa <hoge@etc.com>
2018-02-20 23:48:54 +0900

    ssl関連の変更

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.use('/users', users);
 
 //HTTPS通信で使用するためのSSLキーを設定
 const ssloptions = {
-        key: fs.readFileSync ('/home/tshimakawa/sslKey/privkey5.pem'),
-        cert: fs.readFileSync('/home/tshimakawa/sslKey/cert5.pem'),
-        ca: [fs.readFileSync('/home/tshimakawa/sslKey/chain5.pem'), fs.readFileSync('/home/tshimakawa/sslKey/fullchain5.pem','utf-8')],
+        key: fs.readFileSync ('/home/tshimakawa/sslKey/privkey.pem'),
+        cert: fs.readFileSync('/home/tshimakawa/sslKey/cert.pem'),
+        ca: [fs.readFileSync('/home/tshimakawa/sslKey/chain.pem'), fs.readFileSync('/home/tshimakawa/sslKey/fullchain.pem','utf-8')],
 requestCert: true,
 rejectUnauthorized: false
 };
